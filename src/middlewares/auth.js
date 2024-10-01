5191153843efd9a7837608566740f253bf1c0d86
Vishal Kumar <iamvishukr@gmail.com>
2024-10-01 13:19:53 +0530

    created middleware || signup and login api authentication and validation

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,11 +1,25 @@
-const adminAuth = (req, res, next) => {
-  const token = "xyz";
-  const isAuthenticated = token === "xyz";
-  if (!isAuthenticated) {
-    res.status(401).send("unauthorized");
-  } else {
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+
+const adminAuth = async (req, res, next) => {
+  try {
+    const { token } = req.cookies;
+    if(!token) {
+      throw new Error(" Invalid token");
+    }
+    const decodedObj = jwt.verify(token, "DEV@MATCH$23");
+    const { _id } = decodedObj;
+
+    const user = await User.findById(_id);
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    req.user = user;
     next();
+  } catch (err) {
+    res.status(400).send("Bad Request" + err.message);
   }
 };
 
-module.exports = {adminAuth};
+module.exports = { adminAuth };
--- src/models/user.js
+++ src/models/user.js
@@ -1,24 +1,77 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({   ///schemas for user
     firstName: {
         type: String,
+        required: true,
+        minLength: 3,
+        maxLength: 16,
+        trim: true, // New
+        match: /^[a-zA-Z]+$/,
     },
     lastName: {
         type: String,
+        trim: true, // New
+        match: /^[a-zA-Z]+$/,
     },
     emailId: {
         type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error ("Invalid email address: " + value);
+            }
+        }
     },
     password: {
         type: String,
+        required: true,
+        validate(value){
+            if(!validator.isStrongPassword(value)){
+                throw new Error ("Enter a strong password: " + value);
+            }
+        }
     },
     age: {
         type: Number,
+        min: 18,
+        validate(value) { 
+            if (!Number.isInteger(value)) {
+                throw new Error('Invalid Age');
+            }
+        }
     },
     gender: {
         type: String,
+        validate(value){
+            if(!["male", "female", "other"].includes(value)){
+                throw new Error ("Invalid Gender value");
+            }
+        }
+    }, 
+    photoUrl: {
+        type: String,
+        default: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png',
+        validate(value){
+            if(!validator.isURL(value)){
+                throw new Error ("Invalid Photo URL: " + value);
+            }
+        }
+    },
+    about: {
+        type: String,
+        default: "Hey there, Let's Connect and code together?",
+        maxLength: 200
+    },
+    skills: {
+        type: [String],
     }
+}, {
+    timestamps: true,
 });
 
 const User =  mongoose.model('User', userSchema);  //model for user
