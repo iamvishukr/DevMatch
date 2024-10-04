695dab21ab97d5b48fe5196ad188fc9b9fbb8e11
Vishal Kumar <iamvishukr@gmail.com>
2024-10-04 17:37:41 +0530

    Created Feed API

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
 const { populate } = require("../models/user");
+const User = require("../models/user");
 userRouter = express.Router();
 
 //get all the pending connection request for loggedIn user
@@ -45,6 +46,41 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
       return row.fromUserId;
     });
     res.json({ message: "Data fetched successfully", data });
+  } catch (error) {
+    res.status(400).send("Error: " + error.message);
+  }
+});
+
+userRouter.get("/feed", userAuth, async (req, res) => {
+  try {
+    //User should see all the user cards except
+    //1. His own card
+    //2. his connections
+    //3. ignored cards
+    //4. already sent connection request (interested request)
+
+    const loggedInUser = req.user;
+    //find all the connections requests (sent + received)
+    const connectionRequest = await ConnectionRequest.find({
+      $or: [{ toUserId: loggedInUser._id }, { fromUserId: loggedInUser._id }],
+    }).select("fromUserId toUserId");
+
+    const hideUsersFromFeed = new Set(); // creates array || set of users with no duplicates
+    connectionRequest.forEach((req) => {
+      hideUsersFromFeed.add(req.fromUserId.toString());
+      hideUsersFromFeed.add(req.toUserId.toString());
+    });
+    //console.log(hideUsersFromFeed); // users i don't want to see on my feed,(including myself)
+
+    const users = await User.find({
+      //finding all the users from db whose id is not in hideUsersFromFeed array
+      $and: [
+        { _id: { $nin: Array.from(hideUsersFromFeed) } }, //not include: user cards from hide user
+        { _id: { $ne: loggedInUser._id } }, //not equal : my card (loggedInUser)
+      ],
+    }).select(USER_SAFE_DATA);
+
+    res.send(users);
   } catch (error) {
     res.status(400).send("Error: " + error.message);
   }
